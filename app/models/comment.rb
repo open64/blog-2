678670235ccee62f6e3b10f5b7f6bd05deb5efbc
open64 <systemsofdown@gmail.com>
2014-01-24 23:54:35 +0200

    fix comment author_id

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   validates :content, presence: true
+  def author?(user)
+    if user != nil
+      self[:author_id] == user[:id]
+    else
+      false
+    end
+  end
+
 end
